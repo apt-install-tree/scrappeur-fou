411afb65f6e60df8a977f0d4c57badde6f4f3809
apt-install-tree <62941547+apt-install-tree@users.noreply.github.com>
2020-04-17 03:36:09 +0200

    1

--- spec/00_dark_trader_spec.rb
+++ spec/00_dark_trader_spec.rb
@@ -1,8 +1,5 @@
 require_relative '../lib/00_dark_trader'
 
-# 1) le fonctionnement de base de ton programme (pas d'erreur ni de retour vide)
-# 2) que ton programme sort bien un array cohérent (vérifier la présence de 2-3 cryptomonnaies, vérifier que l’array est de taille cohérente, etc.).
-
 crypto_test = main  # Evite de lancer le test en boucle (+ que 2 fois)
 
 def crypto_present?(coin, crypto_test)
